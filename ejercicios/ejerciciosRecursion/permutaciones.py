aef443c377ede60d71d8b156d3120bf7ec4b9653
ulrich-marcelo <mulrich@estudiantes.unsam.edu.ar>
2024-04-05 17:56:52 -0300

    aparecio mariano y dejo TAREA

---
+++ ejercicios/ejerciciosRecursion/permutaciones.py
@@ -0,0 +1,23 @@
+#dada lista de elementos, necesito lista de las permutaciones que se pueden hacer
+
+def permutaciones(lista:list):
+    combinaciones = []
+    
+
+
+
+
+
+#0. lista vacia de retorno
+#1. permutaciones del tail
+#2. para cada i 0->len(listaOG)-1
+    #3. para cada permutacion de 1. copio como auxiliar, 
+    #inserto la head en la posicion i
+    #4. inserto aux en lista de retorno
+
+#otra manera: agarro para cada uno, lo "saco" de la listaOG y con recursion, lo 
+#agrego de head a cada permutacion de la lista resultante.
+
+
+
+
---
+++ ejercicios/guiaRecursion1/ejercicio24.py
@@ -0,0 +1,27 @@
+#def funcion partes dada list[int] de una lista de listas que sea el conjunto
+#de partes
+
+def partes(lista:list[int])-> list[list[int]]:
+    listaListas = [[]]
+    def partes_interna(lista_interna: list):
+        if not lista_interna:
+            pass
+        else:
+            listaListas.append(lista_interna.copy())
+            for i in lista_interna:
+                
+
+
+
+
+
+
+
+    return [[]]
+
+
+
+
+
+
+
