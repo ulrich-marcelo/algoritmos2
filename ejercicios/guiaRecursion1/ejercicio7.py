c6c89427957505baaac5b8be2f12f2c187051df9
Marchelox2468 <ulrich.marcelo@gmail.com>
2024-04-09 23:45:11 -0300

    Cambio de nombre

---
+++ ejercicios/guiaRecursion1/ejercicio7.py
@@ -0,0 +1,22 @@
+'''
+Definir los siguiente:
+a) Una función recursiva decimalBinario que, dado un número decimal, retorne
+el mismo en base binaria.
+b) Una función recursiva cambioBaseDecimal que, dado un número decimal n y
+un entero b, retorne la conversión del número n en base b.
+c) Una función unosBinario que, dado un número binario (100101), retorne un
+entero indicando la cantidad de 1s que tiene el mismo. El parámetro de la función
+puede ser un entero que respete el formato binario (sólo acepte 0s y 1s).
+d) Una función binarioDecimal que, dado un número binario (100101), retorne el
+mismo número en base decimal (37).
+'''
+
+def decimalBinario(numero : int) -> str:
+    return ''
+
+
+
+
+
+
+
