6d5a0999e7c083553e3a02a9caf053fa16ed66c7
Marchelox2468 <ulrich.marcelo@gmail.com>
2024-04-18 01:01:47 -0300

    progreso ejercicio 7

--- ejercicios/guiaRecursion1/ejercicio7.py
+++ ejercicios/guiaRecursion1/ejercicio7.py
@@ -10,6 +10,7 @@ puede ser un entero que respete el formato binario (sólo acepte 0s y 1s).
 d) Una función binarioDecimal que, dado un número binario (100101), retorne el
 mismo número en base decimal (37).
 '''
+from functools import reduce
 
 def decimalBinario(numero : int) -> str:
     residuos = []
@@ -21,12 +22,35 @@ def decimalBinario(numero : int) -> str:
     _decimalBinario(numero)
     return "".join(residuos)
 
+def cambioBaseDecimal(numero:int,b:int)->str:
+    if not (b <=36 and b>=2):
+        raise ValueError("FLACO PONE UNA BASE VALIDA")
+    residuos = []
+    def _cambio(num:int):
+        if num!=0:
+            residuo = num%b
+            if residuo>=10:
+                residuo = chr(residuo+55)
+            residuos.insert(0,str(residuo))
+            _cambio(num//b)
+    _cambio(numero)
+    return ''.join(residuos)
 
+def unosBinario(binario : str)->int:
+    def es_binario(bina:str)->bool:
+        return bool(reduce(lambda x,y: x*y,map(lambda x: x=='1' or x=='0',bina),True))
 
+    if not es_binario(binario):
+        raise ValueError("El valor ingresado no es un string binario!!")
+    return reduce(lambda x,y: int(x)+int(y),binario,0)
 
-
+def binarioDecimal(binario:str)->int:
+    return 0
 
 print(decimalBinario(16))
+print(cambioBaseDecimal(178,36))
+print(unosBinario('10101110111'))
+
 
 
 
