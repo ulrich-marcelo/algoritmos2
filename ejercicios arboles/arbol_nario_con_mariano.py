e8f795b91218208b18f786bbf0ca7001cd35a42f
Marchelox2468 <ulrich.marcelo@gmail.com>
2024-05-09 23:57:57 -0300

    Ideas Arbol TP

--- ejercicios arboles/arbol_nario_con_mariano.py
+++ ejercicios arboles/arbol_nario_con_mariano.py
@@ -232,3 +232,64 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+
+class Criterion():
+    def calcularImpureza(self,nodo:ArbolN):
+        ...
+    def mejorParticion(self,datos:pd.DataFrame):
+        ...
+    def rankingFeatures(self,datos):
+        ...
+
+class CriterionContiuous(Criterion):
+    def calcularUmbral(self,datos:pd.Dataframe, feature:str):
+        ...
+
+
+class Entropia(CriterionContiuous):
+    def mejorParticion(self, datos: pd.DataFrame):
+        #para cada feature, evaluo si es continuo o categorico y decido
+        '''
+        if categorico:
+            Entropia(D)
+            IG(D) para feature y lo 
+
+        else: contiuo
+            IG() SI() -> GR()
+        
+        A partir de lista de feature + puntaje, elijo max()
+
+        '''
+
+class NodoID3():
+    def __init__(self) -> None:
+        self.criterio = ...
+    def engendrar(self):
+        feature_corte = self.criterio.mejorParticion(self.datos)
+        ...
+
+class NodoC45():
+    def __init__(self) -> None:
+        self.criterio = ...
+    def engendrar(self):
+        feature_corte = self.criterio.mejorParticion(self.datos)
+        umbral = self.criterio.calcularUmbral(self.datos,feature_corte)
+
+ArbolDeDecision(datos,criterio,poda)
+
+
+class ArbolDeDecision():
+    def __init__(self,datos,criterio,poda) -> None:
+        self.raiz = NodoID3(datos,criterio)
+
+    def getCriterio(self):
+        return self.raiz.criterio
+    
+
+
+
+
+
